83e6307e6a3dd893245ed6a0a8cdb0f55b6f9bb3
MauroGITGlobal <maurrx1@gmail.com>
2024-11-26 16:33:45 -0300

    Despliegue en supabase

--- generic_app/settings.py
+++ generic_app/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
+from os import path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tlv)36c^h%me#0j)!67193036i0(q!%)rm_9k19_&32jf9!t3b'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -68,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'generic_app.wsgi.application'
+WSGI_APPLICATION = 'python_vercel_task.wsgi.application'
 
 
 # Database
@@ -77,11 +78,11 @@ WSGI_APPLICATION = 'generic_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'generic_app',
-        'USER': 'sample_user',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': 'postgres',
+        'USER': 'postgres.lqvdfqtqfvhsvazlbtye',
+        'HOST': config('SUPABASE_HOST'),
+        'PASSWORD': config('SUPABASE_PASSWORD'),
+        'PORT': '6543',
     }
 }
 
